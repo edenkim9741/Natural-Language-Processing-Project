979852a37cc985ac90492c6e66b56bba6b6eedcc
eden <edenkim9741@gmail.com>
2025-05-23 01:52:36 +0000

    add dataset make file

---
+++ Dataset/make_dataset.py
@@ -0,0 +1,71 @@
+import json
+
+import os, sys
+
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
+my_modlue_dir = os.path.dirname(os.path.abspath(__file__)) + "/../g2pK"
+sys.path.insert(0, my_modlue_dir)
+
+from g2pk import G2p
+
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm.contrib.concurrent import process_map
+
+
+def process_sentence(args):
+    sentences_w_idx = args
+    g2p = G2p()  # 각 프로세스에서 개별로 생성
+    return g2p(sentences_w_idx, sampling_num=3)
+
+def chunkify(lst, n):
+    return [lst[i:i+n] for i in range(0, len(lst), n)]
+
+json_files = []
+root_dir = "/home/jovyan/Data/001.문서요약/"
+
+for dirpath, _, filenames in os.walk(root_dir):
+    for filename in filenames:
+        if filename.endswith(".json") and 'preprocessed' not in filename:
+            json_files.append(os.path.join(dirpath, filename))
+
+for json_file in json_files:
+    # 예시: json 파일을 불러올 때
+    with open(json_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    # 모든 문장을 담을 리스트
+    all_sentences = []
+
+    # 각 document에 대해 순회
+    for doc in data["documents"]:
+        for paragraph in doc["text"]:  # text는 리스트 안에 리스트 구조
+            for sentence_obj in paragraph:
+                all_sentences.append(sentence_obj["sentence"])
+
+    args_list = [(idx, sentence) for idx, sentence in enumerate(all_sentences)]
+    batched_args = chunkify(args_list, 500)
+
+    # tqdm + 병렬처리 한번에 수행
+    results_nested = process_map(
+        process_sentence,
+        batched_args,
+        max_workers=4,
+        chunksize=1,  # 작은 작업 단위로 설정 (너무 크면 초기 출력이 지연됨)
+        desc="Processing sentences"
+    )
+
+    results = []
+    for batch_result in results_nested:
+        for sent_id, g2pk_result in batch_result.items():
+            results.append({
+                "id": sent_id,
+                "g2p_result": g2pk_result
+            })
+
+
+    result = {"data": results}
+
+
+    with open(json_file.replace(".json", "_preprocessed.json"), "w", encoding="utf-8") as f:
+        json.dump(result, f, ensure_ascii=False, indent=2)
+        
